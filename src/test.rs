b925df06095798201315a36c8411a24c20afb0c1
Joseph Wasden <jpwasden@gmail.com>
2025-03-27 20:41:48 +0000

    Format

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,5 @@
 use crate::results::{Outcome, TestResult};
-use async_std::task::sleep;
 use futures::FutureExt;
-use futures::pin_mut;
 use pyo3::exceptions::PyAssertionError;
 use pyo3::prelude::*;
 use std::future::Future;
@@ -13,14 +11,14 @@ pub struct Test {
     pub id: Uuid,
     pub name: String,
     pub module_name: String,
-    test: Option<Pin<Box<dyn Future<Output=PyResult<PyObject>> + Send>>>,
+    test: Option<Pin<Box<dyn Future<Output = PyResult<PyObject>> + Send>>>,
 }
 
 impl Test {
     pub fn from(
         name: String,
         module_name: String,
-        test: Pin<Box<dyn Future<Output=PyResult<PyObject>> + Send>>,
+        test: Pin<Box<dyn Future<Output = PyResult<PyObject>> + Send>>,
     ) -> Self {
         Self {
             id: Uuid::new_v4(),
@@ -43,49 +41,45 @@ impl Test {
         let test = self.test.take().expect("Ran an empty test").fuse();
 
         match tokio::time::timeout(Duration::from_secs(5), test).await {
-            Ok(outcome) => {
-                match outcome {
-                    Ok(_) => TestResult {
-                        name: Some(&self.name),
-                        module_name: &self.module_name,
-                        test_id: &self.id,
-                        outcome: Outcome::PASSED,
-                        message: None,
-                        tb: None,
-                    },
-                    Err(error) => Python::with_gil(|py| {
-                        if error.is_instance_of::<PyAssertionError>(py) {
-                            TestResult {
-                                name: Some(&self.name),
-                                module_name: &self.module_name,
-                                test_id: &self.id,
-                                outcome: Outcome::FAILED,
-                                message: Some(error.to_string()),
-                                tb: extract_tb(&error, py),
-                            }
-                        } else {
-                            TestResult {
-                                name: Some(&self.name),
-                                module_name: &self.module_name,
-                                test_id: &self.id,
-                                outcome: Outcome::ERRORED,
-                                message: Some(error.to_string()),
-                                tb: extract_tb(&error, py),
-                            }
-                        }
-                    }),
-                }
-            }
-            Err(_) => {
-                TestResult {
-                    test_id: &self.id,
+            Ok(outcome) => match outcome {
+                Ok(_) => TestResult {
                     name: Some(&self.name),
                     module_name: &self.module_name,
-                    outcome: Outcome::TIMEOUT,
-                    message: Some("Timeout after 5 seconds".to_string()),
+                    test_id: &self.id,
+                    outcome: Outcome::PASSED,
+                    message: None,
                     tb: None,
-                }
-            }
+                },
+                Err(error) => Python::with_gil(|py| {
+                    if error.is_instance_of::<PyAssertionError>(py) {
+                        TestResult {
+                            name: Some(&self.name),
+                            module_name: &self.module_name,
+                            test_id: &self.id,
+                            outcome: Outcome::FAILED,
+                            message: Some(error.to_string()),
+                            tb: extract_tb(&error, py),
+                        }
+                    } else {
+                        TestResult {
+                            name: Some(&self.name),
+                            module_name: &self.module_name,
+                            test_id: &self.id,
+                            outcome: Outcome::ERRORED,
+                            message: Some(error.to_string()),
+                            tb: extract_tb(&error, py),
+                        }
+                    }
+                }),
+            },
+            Err(_) => TestResult {
+                test_id: &self.id,
+                name: Some(&self.name),
+                module_name: &self.module_name,
+                outcome: Outcome::TIMEOUT,
+                message: Some("Timeout after 5 seconds".to_string()),
+                tb: None,
+            },
         }
     }
 }
